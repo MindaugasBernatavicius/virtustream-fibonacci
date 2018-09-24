6af68d010b8ed84bd41db8a74d927fbfb915100f
mindaugasb <darbas.mindaugas@gmail.com>
2018-09-24 01:45:25 +0000

    Improvements to setup.h

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
+sudo apt-get update
 sudo apt-get install nginx
-sudo apt-get install python3.6
+sudo apt-get install python3
 sudo apt-get install python3-pip
 sudo apt-get install build-essential
 sudo apt-get install python3-dev
 sudo pip3 install uwsgi
+sudo apt-get install uwsgi-plugin-python3
 sudo pip3 install Flask
 sudo ln -s `pwd`/config/vs-fib-nginx.conf /etc/nginx/sites-enabled/vs-fib.conf
 sudo mkdir /tmp/nginx_cache
+sudo mkdir /var/log/vs-fib/
+sudo pip3 install pytest
