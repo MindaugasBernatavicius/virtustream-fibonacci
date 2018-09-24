303bbe9540c1abda5a29199b3dd7671ddfe01785
mindaugasb <darbas.mindaugas@gmail.com>
2018-09-24 08:06:55 +0000

    Adding autoaccept to setup script

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 sudo apt-get update
-sudo apt-get install nginx
-sudo apt-get install python3
-sudo apt-get install python3-pip
-sudo apt-get install build-essential
-sudo apt-get install python3-dev
-sudo apt-get install uwsgi
-sudo apt-get install uwsgi-plugin-python3
+sudo apt-get -y install nginx
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+sudo apt-get -y install build-essential
+sudo apt-get -y install python3-dev
+sudo apt-get -y install uwsgi
+sudo apt-get -y install uwsgi-plugin-python3
 sudo pip3 install Flask
 sudo ln -s `pwd`/config/vs-fib-nginx.conf /etc/nginx/sites-enabled/vs-fib.conf
 sudo mkdir /tmp/nginx_cache
