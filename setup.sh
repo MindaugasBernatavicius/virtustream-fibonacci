615271cc048bfdd16c4be1c9a0b35f3fa47dd7f9
mindaugasb <darbas.mindaugas@gmail.com>
2018-09-24 00:27:01 +0000

    Adding helper scripts

---
+++ setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get install nginx
+sudo apt-get install python3.6
+sudo apt-get install python3-pip
+sudo apt-get install build-essential
+sudo apt-get install python3-dev
+sudo pip3 install uwsgi
