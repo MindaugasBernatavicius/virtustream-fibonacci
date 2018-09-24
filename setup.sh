2f215398b1fbd50e85964f9c21e9be93440eae42
mindaugasb <darbas.mindaugas@gmail.com>
2018-09-24 00:46:03 +0000

    adding setup helpers

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ sudo apt-get install python3-pip
 sudo apt-get install build-essential
 sudo apt-get install python3-dev
 sudo pip3 install uwsgi
+sudo pip3 install Flask
+sudo ln -s `pwd`/config/vs-fib-nginx.conf /etc/nginx/sites-enabled/vs-fib.conf
+sudo mkdir /tmp/nginx_cache
